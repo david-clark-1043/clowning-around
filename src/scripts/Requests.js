a92e2b7bf11eb7022ed973aa69c6fe6ae4ab4e21
David Clark <david.clark.1043@gmail.com>
2022-02-08 10:31:04 -0600

    Added rendering of request list

--- src/scripts/Requests.js
+++ src/scripts/Requests.js
@@ -1,3 +1,56 @@
+import { getChildren, getParents, getParties, getClowns } from "./dataAccess.js";
+
+
+const mainContainer = document.querySelector("#container")
+
+
+const convertRequestToListElement = (party) => {
+    const clowns = getClowns() 
+    const parents = getParents()
+    const children = getChildren()
+
+    const clownsList = clowns.map(
+        clown => {
+            return `<option value="${party.id}--${clown.id}">${clown.name}</option>`
+        }
+    ).join("") 
+
+    // find parent name
+    const parentName = parents.find(parent => parent.id === party.parentId).name
+
+    // find child name
+    const childName = children.find(child => child.id === party.childId).name
+
+    return `
+    <li class="completed__${party.completed}">
+        <div>Parent: ${parentName}</div>
+        <div>Child: ${childName}</div>
+        <div>Address: ${party.address}</div>
+        <div>Number of Children: ${party.numberOfChildren}</div>
+        <div>Length: ${party.length}</div>
+        <div>Date: ${party.neededBy}</div>
+        <select class="clowns" id="clowns">
+            <option value="">Choose</option>
+            ${clownsList}
+        </select>
+        <button class="request__delete"
+                id="party--${party.id}">
+            Delete
+        </button>
+    </li>
+`
+}
+
 export const Requests = () => {
-    return ""
+    const parties = getParties()
+
+    let html = `
+        <ul>
+            ${
+                parties.map(convertRequestToListElement).join("")
+            }
+        </ul>
+        `
+
+    return html
 }
